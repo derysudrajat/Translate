effc811a284efe7beadc6f4862914ecce150c4f0
derysudrajat <dery.sudrajat17@gmail.com>
2019-06-27 13:59:07 +0700

    Add GUI

--- MendoanTranslate.py
+++ MendoanTranslate.py
@@ -1,8 +1,55 @@
+import sys
+from PyQt5.QtGui import *
+from PyQt5.QtCore import *
+from PyQt5.QtWidgets import *
 from translate import Translator
 
-indoTrans = Translator(to_lang="id")
-engTrans = Translator(from_lang="id", to_lang="en")
-indoTxt = indoTrans.translate("I have made a new technology company that works in the social field")
-engTxt = engTrans.translate(indoTxt)
-print("Indo: \n", indoTxt)
-print("Eng: \n", engTxt)
+### Author
+# @Dery Sudrajat
+
+from PyQt5.QtWidgets import *
+
+
+class TranslateForm(QWidget):
+    def __init__(self):
+        super().__init__()
+        self.setupUI()
+
+    def setupUI(self):
+        self.resize(350, 100)
+        self.move(300, 300)
+        self.setWindowTitle("Mendoan Translate")
+
+        self.leFrom = QTextEdit()
+        font  = self.leFrom.font()
+        font.setPointSize(12)
+        self.leFrom.setFont(font)
+
+        self.btnTranslate = QPushButton("Translate")
+        self.leTo = QTextEdit()
+
+        vbox = QVBoxLayout()
+        vbox.addWidget(self.leFrom)
+        vbox.addWidget(self.btnTranslate)
+        vbox.addWidget(self.leTo)
+
+        self.btnTranslate.clicked.connect(self.Show)
+
+        self.setLayout(vbox)
+
+    def Show(self):
+        indoTrans = Translator(to_lang="id")
+        engTrans = Translator(from_lang="id", to_lang="en")
+        # indoTxt = indoTrans.translate("I was buy new book")
+        engTxt = engTrans.translate(self.leFrom.toPlainText())
+        # print("Indo: \n", indoTxt)
+        self.leTo.setText("<font size = 5>" + engTxt + "</font>")
+
+
+if __name__ == '__main__':
+    a = QApplication(sys.argv)
+
+    form = TranslateForm()
+    form.show()
+
+    a.exec_()
