c006fd6000e00ecce6f98f94b5275cb96e95c683
derysudrajat <dery.sudrajat17@gmail.com>
2019-06-29 00:14:31 +0700

    Add GUI Component to file

--- test/IndoNgapakTrans.py
+++ test/IndoNgapakTrans.py
@@ -1,31 +1,160 @@
 import csv
+import sys
 
-txtInput = "saya pergi ke pasar dengan ayah saya tadi malam"
-data = txtInput.split()
-indo = []
-ngapak = []
-translate = []
-strResult = ""
+from PyQt5.QtWidgets import *
 
-with open('datalang.csv') as csvDataFile:
-    csvReader = csv.reader(csvDataFile)
-    for row in csvReader:
-        indo.append(row[0])
-        ngapak.append(row[1])
 
-for a in data:
-    stat = False
-    for i, x in enumerate(indo):
-        if a == x:
-            translate.append(ngapak[i])
-            stat = True
-    if stat != True:
-        translate.append(a)
+class IndoNgapakForm(QWidget):
+    def __init__(self):
+        super().__init__()
+        self.setupUI()
 
-for j, b in enumerate(translate):
-    strResult += translate[j] + " "
+    def setupUI(self):
+        self.resize(415, 505)
+        self.move(300, 300)
+        self.setWindowTitle("Indo-Ngapak")
+        '''
+            mendefinisikan untuk combo box bahasa awal
+            :param langFrom
+        '''
+        self.langFrom = QComboBox()
+        self.langFrom.addItem("Indonesia")
+        self.langFrom.addItem("Ngapak")
+        '''
+            mendefinisikan Button untuk aksi menukar bahasa
+            pada combo box
+            :param btnSwap
+        '''
+        self.btnSwap = QPushButton("Swap")
+        '''
+            mendefinisikan untuk combo box bahasa tujuan
+            :param langTo
+        '''
+        self.langTo = QComboBox()
+        self.langTo.addItem("Ngapak")
+        self.langTo.addItem("Indonesia")
+        '''
+            pengaturan layout untuk menu menu di atas
+            diatur secara horizontal
+        '''
+        hbox = QHBoxLayout()
+        hbox.addWidget(self.langFrom)
+        hbox.addWidget(self.btnSwap)
+        hbox.addWidget(self.langTo)
+        '''
+            mendefinisikan Text Edit untuk bahasa awal
+            dengan ukuran text 12sp
+            :param leFrom
+        '''
+        self.leFrom = QTextEdit()
+        font = self.leFrom.font()
+        font.setPointSize(12)
+        self.leFrom.setFont(font)
+        '''
+            mendefinisikan Button untuk aksi menerjemahkan text
+            dari bahasa awal ke bahasa tujuan
+            :param btnTranslate
+        '''
+        self.btnTranslate = QPushButton("Translate")
+        '''
+            mendefinisikan Text Edit untuk bahasa awal
+            dengan ukuran text 12sp
+            :param leFrom
+        '''
+        self.leTo = QTextEdit()
+        font2 = self.leTo.font()
+        font2.setPointSize(12)
+        self.leTo.setFont(font2)
+        '''
+            pengaturan layout utama dengan semua komponen
+            diatur secara vertical
+        '''
+        vbox = QVBoxLayout()
+        vbox.addItem(hbox)
+        vbox.addWidget(self.leFrom)
+        vbox.addWidget(self.btnTranslate)
+        vbox.addWidget(self.leTo)
+        '''
+            penanganan aksi untuk semua button pada file ini
+        '''
+        self.btnTranslate.clicked.connect(self.Show)
+        self.btnSwap.clicked.connect(self.swap)
 
-print("Indo:")
-print(txtInput)
-print("Ngapak:")
-print(strResult)
+        self.setLayout(vbox)
+
+    '''
+       method untuk menukar bahasa awal dan bahasa tujuan
+    '''
+
+    def swap(self):
+        txtF = self.langFrom.currentText()
+        txtT = self.langTo.currentText()
+        self.langFrom.setCurrentText(txtT)
+        self.langTo.setCurrentText(txtF)
+
+    '''
+       method untuk mendapatkan string kata yang sudah di
+       terjemahkan
+       :param data # adalah data inputan dari leFrom
+       :param dataFrom # adalah data bahasa awal
+       :param dataTo # adalah data bahasa tujuan 
+    '''
+
+    def nTrans(self, data, dataFrom, dataTo):
+        translate = []
+        strResult = ""
+        for a in data:
+            stat = False
+            for i, x in enumerate(dataFrom):
+                if a == x:
+                    translate.append(dataTo[i])
+                    stat = True
+            if stat != True:
+                translate.append(a)
+
+        for j, b in enumerate(translate):
+            strResult += translate[j] + " "
+        return strResult
+
+    '''
+        menampilkan hasil bahasa yang sudah di terjemahkan ke
+        leTo
+    '''
+
+    def Show(self):
+        txtInput = self.leFrom.toPlainText()
+        txtF = self.langFrom.currentText()
+        data = txtInput.split()
+        print(data)
+        indo = []
+        ngapak = []
+        strResult = ""
+        '''
+        membaca data dari datalang.csv dan menyimpan pada
+        varible indo dan ngapak
+        '''
+        with open('datalang.csv') as csvDataFile:
+            csvReader = csv.reader(csvDataFile)
+            for row in csvReader:
+                indo.append(row[0])
+                ngapak.append(row[1])
+
+        if txtF == "Indonesia":
+            strResult = self.nTrans(data, indo, ngapak)
+        else:
+            strResult = self.nTrans(data, ngapak, indo)
+
+        print(indo)
+        print(ngapak)
+
+        print(strResult)
+        self.leTo.setText(strResult)
+
+
+if __name__ == '__main__':
+    a = QApplication(sys.argv)
+
+    form = IndoNgapakForm()
+    form.show()
+
+    a.exec_()
